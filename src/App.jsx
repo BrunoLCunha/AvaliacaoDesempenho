7558807d874d3b57804685414a7ed0018db88452
Bruno L. Cunha <56527452+BrunoLCunha@users.noreply.github.com>
2021-06-18 19:57:34 -0300

    Page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-import { Button } from 'rsuite';
-import 'rsuite/dist/styles/rsuite-default.css';
+import "./App.css";
+import { Button } from "rsuite";
+import "rsuite/dist/styles/rsuite-default.css";
 import SideMenu from "./components/menu.component";
+import { Container, Header, Content } from "rsuite";
+import Page from "./components/page.component";
 
 function App() {
   return (
     <div className="App">
-        <SideMenu style={{position: "absolute", marginLeft: 0}}/>
-        <Button>Button</Button>
+      <div style={{ float: "left" }}>
+        <SideMenu />
+      </div>
+
+      <Page pageTitle= "Avaliação de desempenho" pageSubtitle= "Aqui você avaliará o desempenho do funcionário Carlos Alberto de Nóbrega">    
+        <br/>
+        <Button>0/10</Button>
+        <Button>10/10</Button>
+      </Page>
     </div>
   );
 }
